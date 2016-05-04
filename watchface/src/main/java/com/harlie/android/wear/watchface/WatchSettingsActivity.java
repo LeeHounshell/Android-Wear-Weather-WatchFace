0cfd9d82f8bd4ac5d140d0b7b12f00e9cf672666
Lee Hounshell <lee.hounshell@gmail.com>
2016-05-04 00:29:19 -0700

    feat: WIP open web page on phone

--- watchface/src/main/java/com/harlie/android/wear/watchface/WatchSettingsActivity.java
+++ watchface/src/main/java/com/harlie/android/wear/watchface/WatchSettingsActivity.java
@@ -1,13 +1,14 @@
 package com.harlie.android.wear.watchface;
 
-import android.app.Notification;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.wearable.activity.ConfirmationActivity;
 import android.util.Log;
 
@@ -17,6 +18,8 @@ public class WatchSettingsActivity extends WearPreferenceActivity
         implements SharedPreferences.OnSharedPreferenceChangeListener
 {
     private final String TAG = "LEE: <" + WatchSettingsActivity.class.getSimpleName() + ">";
+
+    private static final int NOTIFY_ID_LEE_HOUNSHELL = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +34,54 @@ public class WatchSettingsActivity extends WearPreferenceActivity
         Log.v(TAG, "===> onSharedPreferenceChanged - key="+key);
         if (key.equals("contact_lee")) {
             int animation = ConfirmationActivity.OPEN_ON_PHONE_ANIMATION;
-            String message = "http://linkedin.com/pub/lee-hounshell/2/674/852";
-            Log.v(TAG, "===> OPEN ON PHONE: "+message);
+            String webPage = "http://linkedin.com/pub/lee-hounshell/2/674/852";
+            Log.v(TAG, "===> OPEN ON PHONE: "+webPage);
+
+            try {
+                Intent linkedInIntent = new Intent(Intent.ACTION_VIEW);
+                linkedInIntent.setData(Uri.parse(webPage));
+
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+                PendingIntent linkedInPendingIntent = PendingIntent.getActivity(this, 0, linkedInIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+                NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_lee_hounshell)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.lee_hounshell))
+                        .setContentTitle(getString(R.string.lee_hounshell))
+                        .setContentText(getString(R.string.hire_me))
+                        .setContentIntent(linkedInPendingIntent)
+                        .setAutoCancel(true)
+                        .addAction(R.drawable.ic_lee_hounshell, getString(R.string.common_open_on_phone), linkedInPendingIntent);
+
+                notificationManager.notify(NOTIFY_ID_LEE_HOUNSHELL, notificationBuilder.build());
+
+                Intent notifyIntent = new Intent(this, ConfirmationActivity.class);
+                notifyIntent.putExtra(ConfirmationActivity.EXTRA_ANIMATION_TYPE, ConfirmationActivity.OPEN_ON_PHONE_ANIMATION);
+                notifyIntent.putExtra(ConfirmationActivity.EXTRA_MESSAGE, webPage);
+                startActivity(notifyIntent);
+
+                /*
+                NotificationCompat.Action action =
+                    new NotificationCompat.Action.Builder(R.drawable.ic_lee_hounshell, getString(R.string.common_open_on_phone), linkedInPendingIntent)
+                    .build();
+
+                Notification notification = new NotificationCompat.Builder(this)
+                    .setSmallIcon(R.drawable.ic_lee_hounshell)
+                    .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.lee_hounshell))
+                    .setContentTitle(getString(R.string.lee_hounshell))
+                    .setContentText(getString(R.string.hire_me))
+                    .setContentIntent(linkedInPendingIntent)
+                    .setAutoCancel(true)
+                    .extend(new NotificationCompat.WearableExtender().addAction(action))
+                    .build();
+                notificationManager.notify(NOTIFY_ID_LEE_HOUNSHELL, notification);
+                */
+
+                Log.v(TAG, "request sent.");
+            }
+            catch (Exception e) {
+                Log.e(TAG, "LINKEDIN ACTION_VIEW NOTIFICATION ERROR: "+e);
+            }
         }
     }
 }
